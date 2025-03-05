48cafb13e724052615a905589c69b4e4a1472291
PK <doubleobelisk@gmail.com>
2025-03-05 16:35:36 -0500

    added /create-pull and /get-pull/id

--- app.py
+++ app.py
@@ -1,7 +1,104 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from supabase import create_client, Client
+from datetime import datetime, timezone
+import os, psycopg2, json, requests, random, string
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+with open('decks.json') as f:
+    decks = json.load(f)
+
+RANDOMORG_KEY = os.environ.get("RANDOMORG_KEY")
+RANDOMORG_HOST = 'https://api.random.org/json-rpc/4/invoke'
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+def generateRandomString(length):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+def generateIntegerSequences(length, totalCards):
+    body = {
+        'jsonrpc': '2.0',
+        'method': 'generateIntegerSequences',
+        'params': {
+            'apiKey': RANDOMORG_KEY,
+            'n': 2,
+            'length': length,
+            "min": [1, 0],
+            "max": [totalCards, 1],
+            "replacement": [False, True]
+        },
+        "id": random.randint(10000, 99999)
+    }
+    message = requests.post(RANDOMORG_HOST, json=body).json()
+    return message
+
+def pullCards(arrays, deck, allowReversed):
+    cards = []
+    for n in range(0,len(arrays[0])):
+        card = decks[deck]['cards'][arrays[0][n]-1]
+        if arrays[1][n] == 1 and allowReversed:
+            card['reversed'] = True
+        else:
+            card['reversed'] = False
+        cards.append(card)
+    return cards
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message':'Hello, world','success':'true'})
+
+# example json body for create-pull
+# {
+#     "spreadType":"Single", # "Triple" or "Celtic Cross" - anything as long as it's implemented in front end
+#     "numberOfCards":1, # or 3 or 6 or whatever
+#     "deck":"Rider-Waite-Smith", # currently the only deck
+#     "allowReversed":true,
+#     "intention":"What will the weather be tomorrow?", # a user defined string, optional
+# }
+
+@app.route('/create-pull', methods=['POST'])
+def createPull():
+    pullId = generateRandomString(10)
+    pullDetails = request.json
+    pull = {
+        'pullDetails': pullDetails,
+        'id': pullId,
+        'timestamp': str(datetime.now(timezone.utc))
+    }
+
+    # call to random.org api
+    randomOrgResponse = generateIntegerSequences(pullDetails['numberOfCards'], decks[pullDetails['deck']]['totalCards'])
+    randomInts = []
+    # check random.org api response for errors - perhaps should add a fallback using math.random
+    if 'result' in randomOrgResponse:
+        randomInts = randomOrgResponse['result']['random']['data']
+    elif 'error' in randomOrgResponse:
+        return jsonify({'message':{'randomOrgResponse': randomOrgResponse},'success':'false'})
+    else:
+        return jsonify({'message':'unknown error','success':'false'})
+
+    # pull cards according to the numbers from random.org
+    pull['cards'] = pullCards(randomInts, pullDetails['deck'], pullDetails['allowReversed'])
+
+    # store pull in the supabase
+    supabaseResponse = (
+        supabase.table("pulls")
+        .insert({"id": pullId, "pull": pull})
+        .execute()
+    )
+
+    return jsonify({'message':pull,'success':'true'})
+
+@app.route('/get-pull/<string:id>', methods=['GET'])
+def getPull(id):
+    response = (
+        supabase.table("pulls")
+        .select("*")
+        .eq("id", id)
+        .execute()
+    )
+    print(response.json())
+    return jsonify(response.json())
---
+++ app_psycop2_db.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request
+import os, psycopg2, json, requests
+
+app = Flask(__name__)
+
+with open('decks.json') as f:
+    decks = json.load(f)
+
+databaseUrl = os.environ['DATABASE_URL']
+conn = psycopg2.connect(databaseUrl)
+
+class Pull(db.Model):
+    id = db.Column(db.String(20), primary_key=True)
+    spread_type = db.Column(db.String(20), unique=False, nullable=False)
+    number_of_cards = db.Column(db.Integer, nullable=False)
+    intention = db.Column(db.String(100), unique=False, nullable=False)
+    created_time = db.Column(db.DateTime, unique=False, nullable=False)
+
+
+
+def pullRandomCards():
+    return "cards"
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message':'Hello, world','success':'true'})
+
+@app.route('/create-pull', methods=['POST'])
+def createPull():
+    pullDetails = request.json
+    spreadType = pullDetails['spreadType']
+    numberOfCards = pullDetails['numberOfCards']
+    deck = pullDetails['deck']
+    intention = pullDetails['intention']
+    pull = {
+        'pullDetails': pullDetails
+    }
+    # for i in range(numberOfCards):
+    #     pull['cards'][i] = 
+    return jsonify({'message':pull,'success':'true'})
